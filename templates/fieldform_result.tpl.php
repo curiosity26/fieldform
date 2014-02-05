0504de7cb9c4997aac0477855bf7676980fa288c
Alex Boyce <curiosity26@gmail.com>
2014-02-05 17:35:31 -0500

    Finalized Email field options. Nixed auto-login for existing accounts due to security issues. Did allow new accounts to be created and auto-login (as well as shoot out the default registration email). On to the Table element and confirming metawrapper results.

--- templates/fieldform_result.tpl.php
+++ templates/fieldform_result.tpl.php
@@ -7,10 +7,10 @@
  */
 ?>
 <div <?php print $attributes ?> class="<?php print $classes ?>">
-  <<?php if($title_display === 'before'):print 'span';else:print 'div';endif;?> <?php print $title_attributes ?>>
+  <h3 <?php print $title_attributes ?>>
     <?php print render($title_prefix).$title.render($title_suffix) ?>
-  </<?php if($title_display === 'before'):print 'span';else:print 'div';endif;?>>
-  <<?php if($title_display === 'before'):print 'span';else:print 'div';endif;?> <?php print $content_attributes?>>
+  </h3>
+  <div <?php print $content_attributes?>>
     <?php print $content ?>
-  </<?php if($title_display === 'before'):print 'span';else:print 'div';endif;?>>
+  </div>
 </div>
