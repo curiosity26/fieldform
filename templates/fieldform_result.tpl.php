a95b99af4cfc3c04216d36f4f2c2f6821e4c18d3
Alex Boyce <curiosity26@gmail.com>
2014-02-10 13:32:37 -0500

    Child elements for a FieldForm aren't saving their results

--- templates/fieldform_result.tpl.php
+++ templates/fieldform_result.tpl.php
@@ -7,7 +7,7 @@
  */
 ?>
 <div <?php print $attributes ?> class="<?php print $classes ?>">
-  <?php if ($title): ?>
+  <?php if (isset($title)): ?>
     <h3 <?php print $title_attributes ?>>
       <?php print render($title_prefix).$title.render($title_suffix) ?>
     </h3>
