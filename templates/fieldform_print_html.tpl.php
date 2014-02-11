dd66eec6fb6609223196368366433c242c876645
Alex Boyce <curiosity26@gmail.com>
2014-02-11 17:27:33 -0500

    Minor alterations to allow for more flexible themability

--- templates/fieldform_print_html.tpl.php
+++ templates/fieldform_print_html.tpl.php
@@ -5,4 +5,25 @@
  * Date: 2/11/14
  * Time: 4:31 PM
  */
+?>
 
+<?php if ($html5): ?>
+<!DOCTYPE html>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
+<?php else: ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
+"http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
+<?php endif; ?>
+<head <?php if(!$html5):print 'profile="'.$grddl_profile.'"';endif; ?>>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+<?php print $page_top; ?>
+<?php print $page; ?>
+<?php print $page_bottom; ?>
+</body>
+</html>
