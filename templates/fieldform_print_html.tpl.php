e91478660c4331d43252ad345019cc999e89fe37
Alex Boyce <curiosity26@gmail.com>
2014-02-11 17:37:49 -0500

    Minor alterations to allow for more flexible themability

--- templates/fieldform_print_html.tpl.php
+++ templates/fieldform_print_html.tpl.php
@@ -6,22 +6,20 @@
  * Time: 4:31 PM
  */
 ?>
-
-<?php if ($html5): ?>
-<!DOCTYPE html>
-<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
-<?php else: ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
-"http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
-<?php endif; ?>
-<head <?php if(!$html5):print 'profile="'.$grddl_profile.'"';endif; ?>>
+
+<head profile="<?php print $grddl_profile; ?>">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+<div id="skip-link">
+  <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+</div>
 <?php print $page_top; ?>
 <?php print $page; ?>
 <?php print $page_bottom; ?>
