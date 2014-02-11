5aabff9d166731410d05bf809896c7576b5899cd
Alex Boyce <curiosity26@gmail.com>
2014-02-11 17:41:21 -0500

    Minor alterations to allow for more flexible themability

--- templates/fieldform_print_html.tpl.php
+++ templates/fieldform_print_html.tpl.php
@@ -6,9 +6,8 @@
  * Time: 4:31 PM
  */
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
-  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
+<!DOCTYPE html>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
 
 <head profile="<?php print $grddl_profile; ?>">
   <?php print $head; ?>
@@ -17,9 +16,6 @@
   <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
-<div id="skip-link">
-  <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-</div>
 <?php print $page_top; ?>
 <?php print $page; ?>
 <?php print $page_bottom; ?>
